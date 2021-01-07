74b9af5cde6c544cb3adc6fdaf26ab24cf4e4773
Nicolo Greggio <nicolo.greggio.1@studenti.unipd.it>
2021-01-07 18:35:44 +0100

    improved scripts, trying to fix extra latex packages  problems

--- .github/workflows/glossary_builder.sh
+++ .github/workflows/glossary_builder.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd `dirname $0`
+
 #install extra latex packages
 if [ $# -gt 0 ]
 then
@@ -35,7 +37,7 @@ do
 			sing=`echo $line | cut -f3 -d ':'`
 			plur=`echo $line | cut -f4 -d ':'`
 			mean=`echo $line | cut -f5 -d ':'`
-			if [ `grep -c "newglossaryentry{$key" glossario.tex` -lt 1 ]
+			if [ `grep -c "newglossaryentry{$key}" glossario.tex` -lt 1 ]
 			then
 				echo "missing G: $key, adding it"
 				
@@ -54,7 +56,9 @@ do
 				for f in `grep -ril "$plur" | grep '\.tex' | grep -v glossario`
 				do
 					# lookbehind needed (?<!gls{)
-					perl -i -p -e "s/(?<!(ls|pl|rt)\{)$plur(?!{)/\\\glspl{$key}\\\textsubscript{G}/gi" $f
+					#perl -i -p -e "s/(?<!(ls|pl|rt)\{)$plur(?!{)/\\\glspl{$key}\\\textsubscript{G}/gi" $f
+					#perl -i -p -e "s/(?<!(ls\{|pl\{|rt\{|[a-z]{3}))$plur(?!\{)/\\\glspl{$key}\\\textsubscript{G}/g" $f
+					perl -i -p -e "s/(?<=\s)$plur(?=\s|\.|\:|,|')/\\\glspl{$key}\\\textsubscript{G}/g" $f
 					echo "found in $f"
 					#cat $f | perl -p -e "s/(?<!gls{)$plur/\\\glspl{$key}/g"
 				done
@@ -65,7 +69,9 @@ do
 			for f in `grep -ril "$sing" | grep '\.tex' | grep -v glossario`
 			do
 				# lookbehind needed (?<!gls{)
-				perl -i -p -e "s/(?<!(ls|pl|rt)\{)$sing(?!{)/\\\gls{$key}\\\textsubscript{G}/gi" $f
+				#perl -i -p -e "s/(?<!(ls|pl|rt)\{)$sing(?!{)/\\\gls{$key}\\\textsubscript{G}/gi" $f
+				#perl -i -p -e "s/(?<!(ls\{|pl\{|rt\{|[a-z]{3}))$sing(?!\{)/\\\gls{$key}\\\textsubscript{G}/g" $f
+				perl -i -p -e "s/(?<=\s)$sing(?=\s|\.|\:|,|')/\\\gls{$key}\\\textsubscript{G}/g" $f
 				echo "found in $f"
 				#cat $f | perl -p -e "s/(?<!gls{)$sing/\\\gls{$key}/g"
 			done
@@ -85,7 +91,10 @@ do
 			echo "checking $short occurencies..."
 			for f in `grep -ril "$short" | grep '\.tex' | grep -v glossario`
 			do
-				perl -i -p -e "s/(?<!(ls|pl|rt|[a-z]{2})\{)$short(?!{)/\\\acrshort{$key}\\\textsubscript{A}/gi" $f
+				#(?<!(ls\{|pl\{|rt\{|[a-z]{3}))$WORD(?!\{)
+				#perl -i -p -e "s/(?<!(ls\{|pl\{|rt\{|[a-z]{3}))$short(?!\{)/\\\acrshort{$key}\\\textsubscript{A}/g" $f
+				perl -i -p -e "s/(?<=\s)$short(?=(\s|\.|\:|'))/\\\acrshort{$key}\\\textsubscript{A}/g" $f
+				#perl -i -p -e "s/(?<!(ls|pl|rt|[a-z]{2})\{)$short(?!{)/\\\acrshort{$key}\\\textsubscript{A}/gi" $f
 				echo "found in $f"
 				#cat $f | perl -p -e "s/(?<!acrshort{)$short(?!{)/\\\acrshort{$key}/g"
 			done
