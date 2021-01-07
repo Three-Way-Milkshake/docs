541dcb7f18912d925b374f6ef4334751c6a6d7cd
Nicolo Greggio <nicolo.greggio.1@studenti.unipd.it>
2021-01-07 18:32:17 +0100

    trying to fix compilation problem due to plots and pie packages, edited scripts accordingly and added required packages

--- .github/workflows/glossary_builder.sh
+++ .github/workflows/glossary_builder.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+#install extra latex packages
+if [ $# -gt 0 ]
+then
+	if [ $1 == "--install-custom" ]
+	then
+		sudo cp -rv extra_latex_packages/pgfplots /usr/share/texlive/texmf-dist/tex/latex
+		sudo cp -rv extra_latex_packages/pgf-pie /usr/share/texlive/texmf-dist/tex/latex
+		sudo mktexlsr
+	fi
+fi
+
 cd ../../
 
 for doc in `find . -name glossario.txt`
