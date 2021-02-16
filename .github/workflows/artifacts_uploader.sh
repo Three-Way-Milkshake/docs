74056dd6a40463f5ac9868c8fa30bc4cf7b3e81a
Nicolo Greggio <nicolo.greggio.1@studenti.unipd.it>
2021-02-16 23:08:24 +0100

    added some logs

--- .github/workflows/artifacts_uploader.sh
+++ .github/workflows/artifacts_uploader.sh
@@ -7,24 +7,31 @@ then
 	# installs gupload utils script
 	if [ $1 == "-i" ]
 	then
+	    echo "Installing google-driveupload script (https://github.com/labbots/google-drive-upload)..."
 		./google-drive-upload/install.sh
+		echo "Done."
 	fi
 	
 	# create config using secrets
 	if [ $2 == "-c" ]
 	then
+    	echo "configuring drive env with secrets..."
 		cp drive_conf.template cnf
 		sed -i "s/CID/$CID/" cnf
 		sed -i "s/CSEC/$CSEC/" cnf
 		sed -i "s/RTOKEN/$RTOKEN/" cnf
 		sed -i "s/ROOT_FLD/$ROOT_FLD/" cnf
 		mv cnf ~/.googledrive.conf
+		echo "Done."
 	fi
 fi
 
 cd ../../
 
+echo "Uploading pdf files to gdrive..."
 find . -name '*.pdf' | while read file
 do
 	gupload $file
 done
+
+echo "Completed!"
